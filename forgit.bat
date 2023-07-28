5983ab2e5a7a5aee96e731f3e583f147cc95a9c5
bsureshkrishna <98675732+bsureshkrishna@users.noreply.github.com>
2023-07-28 18:24:20 -0400

    reverting

--- forgit.bat
+++ forgit.bat
@@ -1,10 +1,12 @@
+IF [%1] == [], (set msg="minor change") else (set msg=%1)
+echo %msg%
 call quarto.cmd render
 git add -A
-git commit -m "minor change"
+git commit -m %msg%
 git push
 cd ../fr
 call quarto.cmd render
 git add -A
-git commit -m "minor change"
-git push 
+git commit -m %msg%
+git push
 cd ../m2b3en
